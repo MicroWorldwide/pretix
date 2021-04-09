12118521bbf48e2338ab711d11b9a1d17ab5bff7
Raphael Michel <mail@raphaelmichel.de>
2021-04-09 14:19:14 +0200

    Use memberships in orders

--- src/pretix/control/views/organizer.py
+++ src/pretix/control/views/organizer.py
@@ -1759,7 +1759,7 @@ class CustomerDetailView(OrganizerDetailViewMixin, OrganizerPermissionRequiredMi
 
         for m in ctx['memberships']:
             if m.membership_type.max_usages:
-                m.percent = m.usage / m.membership_type.max_usages
+                m.percent = int(m.usages / m.membership_type.max_usages * 100)
             else:
                 m.percent = 0
 
